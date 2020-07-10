57344cf3704b9cd03ec81d043f397c15fb5ee653
lyf2000 <lyf2000@mail.ru>
2020-07-10 23:05:14 +0300

    create Author Model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    patronymic = models.CharField(max_length=64)
