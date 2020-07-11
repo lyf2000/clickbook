a302eae7eecc7e1e23f3756f7f2d19fdfc6ee274
lyf2000 <lyf2000@mail.ru>
2020-07-11 09:15:48 +0300

    Create Book Model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.ForeignKey('users.Author', on_delete=models.CASCADE, related_name='books')
+    name = models.CharField(max_length=256)
+    cost = models.PositiveIntegerField()
+
+    def clean(self):
+        if self.cost == 0:
+            raise ValidationError(
+                _('Zero-cost book is not available!')
+            )
