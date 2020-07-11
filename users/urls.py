14fe6331c3a6d71b49f68f9e9a295b546a7f63b7
lyf2000 <lyf2000@mail.ru>
2020-07-11 12:16:54 +0300

    change login url

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='user-create'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('authors/', AuthorListView.as_view(), name='author-list'),
 
