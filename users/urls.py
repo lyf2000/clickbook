27603d280cec6cd48775258da406078ebd07fc35
lyf2000 <lyf2000@mail.ru>
2020-07-11 09:17:10 +0300

    Create Authors View

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import AuthorListView
+
 app_name = 'users'
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('authors/', AuthorListView.as_view(), name='author-list'),
 
 ]
