434b018230433762adf652b37d821011dc2a0eb5
lyf2000 <lyf2000@mail.ru>
2020-07-11 10:28:48 +0300

    small fix

--- books/helper.py
+++ books/helper.py
@@ -1,4 +1,4 @@
- def get_email_message(order):
+def get_email_message(order):
     return f'USER {order.client.username}, PHONE: {order.call}, ordered BOOK ' \
            f'{order.book.name}(id: {order.book.id}). at {order.created} ' \
            f'Order id: {order.id}.'
